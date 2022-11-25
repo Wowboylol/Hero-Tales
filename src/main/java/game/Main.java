de1dd6b870fcb4353d862bc9f26a2bf5ed15955e
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-24 18:26:18 -0800

    Initial setup of Main class

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,13 +1,23 @@
-package game;
-
-/**
- * Hello world!
+/*  
+ *  Main.java
+ *  
+ *  Description: Sets up window of game and runs Simulator.
  *
- */
+*/
+
+package game;
+import javax.swing.JFrame;
+
 public class Main 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Setting up window properties
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Hero Tales");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
