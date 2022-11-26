ddcf516c3719a57f4ae74c6a3c37f4384792ea14
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-26 02:11:18 -0800

    Implemented Entity class

---
+++ src/main/java/entities/Entity.java
@@ -0,0 +1,30 @@
+/*  
+ *  Entity.java
+ *  
+ *  Description: Abstract class, super class for players, enemies, objects, heroes, etc.
+ *
+*/
+
+package entities;
+
+public abstract class Entity 
+{
+    // Attributes
+    private Coordinate position;
+    
+    // Default constructor
+    public Entity(int setX, int setY)
+    {
+        this.position = new Coordinate(setX, setY);
+    }
+
+    // Getters
+    public Coordinate getCoordinate() { return this.position; }
+    public int getCoordinateX() { return this.position.get_X(); }
+    public int getCoordinateY() { return this.position.get_Y(); }
+
+    // Setters
+    public void setCoordinate(Coordinate coordinate) { this.position = coordinate; }
+    public void setCoordinateX(int X) { this.position.set_X(X); }
+    public void setCoordinateY(int Y) { this.position.set_Y(Y); }
+}
