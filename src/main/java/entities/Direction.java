91bf3eeaa66cb8154529bb970618da4be5f0cb07
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-25 18:41:45 -0800

    Created Direction enum

---
+++ src/main/java/entities/Direction.java
@@ -0,0 +1,16 @@
+/*  
+ *  Direction.java
+ *  
+ *  Description: Enum for the four cardinal directions.
+ *
+*/
+
+package entities;
+
+public enum Direction 
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
