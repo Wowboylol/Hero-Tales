2328b3ccc672e57c02114b4bac722fd79209a2b7
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-26 15:56:41 -0800

    Created AnimateEntity class skeleton

---
+++ src/main/java/entities/AnimateEntity.java
@@ -0,0 +1,27 @@
+/*  
+ *  AnimateEntity.java
+ *  
+ *  Description: Abstract class, inherits Entity.
+ *
+*/
+
+package entities;
+
+public abstract class AnimateEntity extends Entity
+{
+    // Attributes
+    private int moveSpeed;
+
+    // Default constructor
+    public AnimateEntity(int setX, int setY)
+    {
+        super(setX, setY);
+        this.moveSpeed = 3;
+    }
+
+    // Getters
+    public int getMoveSpeed() { return this.moveSpeed; }
+
+    // Setters
+    public void setMoveSpeed(int speed) { this.moveSpeed = speed; }
+}
