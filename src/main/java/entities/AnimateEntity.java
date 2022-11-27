a2ad337bc4dde287482a1e683888478181d22e6f
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-26 22:53:51 -0800

    Render player sprite with no animation

--- src/main/java/entities/AnimateEntity.java
+++ src/main/java/entities/AnimateEntity.java
@@ -6,22 +6,28 @@
 */
 
 package entities;
+import java.awt.image.BufferedImage;
 
 public abstract class AnimateEntity extends Entity
 {
-    // Defaults
+    // Attributes
     private int moveSpeed;
+    private Direction direction;
+    protected BufferedImage up1, up2, up3, down1, down2, down3, left1, left2, left3, right1, right2, right3;
 
     // Default constructor
     public AnimateEntity(int setX, int setY)
     {
         super(setX, setY);
         this.moveSpeed = 3;
+        this.direction = Direction.DOWN;
     }
 
     // Getters
     public int getMoveSpeed() { return this.moveSpeed; }
+    public Direction getDirection() { return this.direction; }
 
     // Setters
     public void setMoveSpeed(int speed) { this.moveSpeed = speed; }
+    public void setDirection(Direction d) { this.direction = d; }
 }
--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -8,7 +8,9 @@
 package entities;
 import main.*;
 import java.awt.Graphics2D;
-import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Player extends AnimateEntity
 {
@@ -27,6 +29,9 @@ public class Player extends AnimateEntity
         super(SPAWN_X, SPAWN_Y);
         this.sim = sim;
         this.keyboard = sim.getKeyboard();
+        this.getSprite();
+        
+        // Super class overriding
         this.setMoveSpeed(BASE_MOVE_SPEED);
     }
 
@@ -35,18 +40,22 @@ public class Player extends AnimateEntity
     {
         if(keyboard.getDirection(Direction.UP) == true)
         {
+            setDirection(Direction.UP);
             this.setCoordinateY(getCoordinateY() - getMoveSpeed());
         }
         if(keyboard.getDirection(Direction.DOWN) == true)
         {
+            setDirection(Direction.DOWN);
             this.setCoordinateY(getCoordinateY() + getMoveSpeed());
         }
         if(keyboard.getDirection(Direction.LEFT) == true)
         {
+            setDirection(Direction.LEFT);
             this.setCoordinateX(getCoordinateX() - getMoveSpeed());
         }
         if(keyboard.getDirection(Direction.RIGHT) == true)
         {
+            setDirection(Direction.RIGHT);
             this.setCoordinateX(getCoordinateX() + getMoveSpeed());
         }
     }
@@ -54,7 +63,46 @@ public class Player extends AnimateEntity
     // Draw the class in window via the Simulator
     public void draw(Graphics2D g2)
     {
-        g2.setColor(Color.white);
-        g2.fillRect(getCoordinateX(), getCoordinateY(), sim.getTileSize(), sim.getTileSize());
+        BufferedImage image = null;
+        switch(this.getDirection())
+        {
+            case UP:
+                image = up1;
+                break;
+            case DOWN:
+                image = down1;
+                break;
+            case LEFT:
+                image = left1;
+                break;
+            case RIGHT:
+                image = right1;
+                break;
+        }
+        g2.drawImage(image, getCoordinateX(), getCoordinateY(), sim.getTileSize(), sim.getTileSize(), null);
+    }
+
+    // Load player sprites into BufferedImage, returns true if successful
+    public boolean getSprite()
+    {
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/player_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/player_up_2.png"));
+            up3 = ImageIO.read(getClass().getResourceAsStream("/player/player_up_3.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/player_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/player_down_2.png"));
+            down3 = ImageIO.read(getClass().getResourceAsStream("/player/player_down_3.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/player_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/player_left_2.png"));
+            left3 = ImageIO.read(getClass().getResourceAsStream("/player/player_left_3.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/player_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/player_right_2.png"));
+            right3 = ImageIO.read(getClass().getResourceAsStream("/player/player_right_3.png"));
+            return true;
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
