ddcbf8b239d5950e51574a4f1843370c9ed19c49
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-12-09 00:38:24 -0800

    Updated map

--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -16,7 +16,7 @@ import javax.imageio.ImageIO;
 public class Player extends AnimateEntity
 {
     // Configurations
-    public static final Coordinate PLAYER_SPAWN_POSITION = new Coordinate(Simulator.TILE_SIZE*8, Simulator.TILE_SIZE*8);
+    public static final Coordinate PLAYER_SPAWN_POSITION = new Coordinate(Simulator.TILE_SIZE*6, Simulator.TILE_SIZE*42);
     public static final int PLAYER_SCREEN_X = Simulator.SCREEN_WIDTH/2 - (Simulator.TILE_SIZE/2);
     public static final int PLAYER_SCREEN_Y = Simulator.SCREEN_HEIGHT/2 - (Simulator.TILE_SIZE/2);
     public final Rectangle HITBOX_CONFIGURATIONS = new Rectangle(9, 12, 30, 30);
