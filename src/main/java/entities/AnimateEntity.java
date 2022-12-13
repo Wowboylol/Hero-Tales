a342e8e9b35288420e76f3ef13c49d694ef8fd4e
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-12-12 16:44:23 -0800

    Implemented character attack animation of mouse

--- src/main/java/entities/AnimateEntity.java
+++ src/main/java/entities/AnimateEntity.java
@@ -12,6 +12,7 @@ public abstract class AnimateEntity extends Entity
 {
     // Attributes
     protected BufferedImage up1, up2, up3, down1, down2, down3, left1, left2, left3, right1, right2, right3;
+    protected BufferedImage attackUp, attackDown, attackLeft, attackRight;
     private int actionCount = 0;
     private int spriteNum = 1;
     private boolean isMoving = false;
--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -104,60 +104,76 @@ public class Player extends AnimateEntity
     public void draw(Graphics2D graphics2D)
     {
         BufferedImage image = null;
+        int drawPositionX = playerScreenPositionX();
+        int drawPositionY = playerScreenPositionY();
 
         if(mouse.getMousePressed())
         {
-            System.out.println(mouse.getAttackDirection(playerScreenPositionX()+getOriginPointX(), playerScreenPositionY()+getOriginPointY()));
+            switch(mouse.getAttackDirection(playerScreenPositionX()+getOriginPointX(), playerScreenPositionY()+getOriginPointY()))
+            {
+                case UP:
+                    if(this.getSpriteNum()%2 != 0) drawPositionY = playerScreenPositionY() - Simulator.TILE_SIZE;
+                    if(this.getSpriteNum() == 1) image = attackUp;
+                    if(this.getSpriteNum() == 2) image = up1;
+                    if(this.getSpriteNum() == 3) image = attackUp;
+                    if(this.getSpriteNum() == 4) image = up1;
+                    break;
+                case DOWN:
+                    if(this.getSpriteNum() == 1) image = attackDown;
+                    if(this.getSpriteNum() == 2) image = down1;
+                    if(this.getSpriteNum() == 3) image = attackDown;
+                    if(this.getSpriteNum() == 4) image = down1;
+                    break;
+                case LEFT:
+                    if(this.getSpriteNum()%2 != 0) drawPositionX = playerScreenPositionX() - Simulator.TILE_SIZE;
+                    if(this.getSpriteNum() == 1) image = attackLeft;
+                    if(this.getSpriteNum() == 2) image = left1;
+                    if(this.getSpriteNum() == 3) image = attackLeft;
+                    if(this.getSpriteNum() == 4) image = left1;
+                    break;
+                case RIGHT:
+                    if(this.getSpriteNum() == 1) image = attackRight;
+                    if(this.getSpriteNum() == 2) image = right1;
+                    if(this.getSpriteNum() == 3) image = attackRight;
+                    if(this.getSpriteNum() == 4) image = right1;
+                    break;
+            }
         }
-
-        switch(this.getDirection())
+        else
         {
-            case UPRIGHT:
-            case UPLEFT:
-            case UP:
-                if(this.getSpriteNum() == 1)
-                    image = up1;
-                if(this.getSpriteNum() == 2)
-                    image = up2;
-                if(this.getSpriteNum() == 3)
-                    image = up1;
-                if(this.getSpriteNum() == 4)
-                    image = up3;
-                break;
-            case DOWNRIGHT:
-            case DOWNLEFT:
-            case DOWN:
-                if(this.getSpriteNum() == 1)
-                    image = down1;
-                if(this.getSpriteNum() == 2)
-                    image = down2;
-                if(this.getSpriteNum() == 3)
-                    image = down1;
-                if(this.getSpriteNum() == 4)
-                    image = down3;
-                break;
-            case LEFT:
-                if(this.getSpriteNum() == 1)
-                    image = left1;
-                if(this.getSpriteNum() == 2)
-                    image = left2;
-                if(this.getSpriteNum() == 3)
-                    image = left1;
-                if(this.getSpriteNum() == 4)
-                    image = left3;
-                break;
-            case RIGHT:
-                if(this.getSpriteNum() == 1)
-                    image = right1;
-                if(this.getSpriteNum() == 2)
-                    image = right2;
-                if(this.getSpriteNum() == 3)
-                    image = right1;
-                if(this.getSpriteNum() == 4)
-                    image = right3;
-                break;
+            switch(this.getDirection())
+            {
+                case UPRIGHT:
+                case UPLEFT:
+                case UP:
+                    if(this.getSpriteNum() == 1) image = up1;
+                    if(this.getSpriteNum() == 2) image = up2;
+                    if(this.getSpriteNum() == 3) image = up1;
+                    if(this.getSpriteNum() == 4) image = up3;
+                    break;
+                case DOWNRIGHT:
+                case DOWNLEFT:
+                case DOWN:
+                    if(this.getSpriteNum() == 1) image = down1;
+                    if(this.getSpriteNum() == 2) image = down2;
+                    if(this.getSpriteNum() == 3) image = down1;
+                    if(this.getSpriteNum() == 4) image = down3;
+                    break;
+                case LEFT:
+                    if(this.getSpriteNum() == 1) image = left1;
+                    if(this.getSpriteNum() == 2) image = left2;
+                    if(this.getSpriteNum() == 3) image = left1;
+                    if(this.getSpriteNum() == 4) image = left3;
+                    break;
+                case RIGHT:
+                    if(this.getSpriteNum() == 1) image = right1;
+                    if(this.getSpriteNum() == 2) image = right2;
+                    if(this.getSpriteNum() == 3) image = right1;
+                    if(this.getSpriteNum() == 4) image = right3;
+                    break;
+            }
         }
-        graphics2D.drawImage(image, playerScreenPositionX(), playerScreenPositionY(), null);
+        graphics2D.drawImage(image, drawPositionX, drawPositionY, null);
     }
 
     // Load player sprites into BufferedImage, returns true if successful
@@ -175,6 +191,10 @@ public class Player extends AnimateEntity
         right1 = spriteSetup("player_right_1", Simulator.TILE_SIZE, Simulator.TILE_SIZE);
         right2 = spriteSetup("player_right_2", Simulator.TILE_SIZE, Simulator.TILE_SIZE);
         right3 = spriteSetup("player_right_3", Simulator.TILE_SIZE, Simulator.TILE_SIZE);
+        attackUp = spriteSetup("player_attack_up", Simulator.TILE_SIZE, Simulator.TILE_SIZE*2);
+        attackDown = spriteSetup("player_attack_down", Simulator.TILE_SIZE, Simulator.TILE_SIZE*2);
+        attackLeft = spriteSetup("player_attack_left", Simulator.TILE_SIZE*2, Simulator.TILE_SIZE);
+        attackRight = spriteSetup("player_attack_right", Simulator.TILE_SIZE*2, Simulator.TILE_SIZE);
     }
 
     // Helper function: Sets up player sprites by resizing image from file
@@ -193,8 +213,14 @@ public class Player extends AnimateEntity
         return image;
     }
 
-    // Helper function: Determines whether player is moving in given frame
-    private void isMovingSetter() { this.setIsMoving(keyboard.isWASDPressed()); }
+    // Helper function: Determines whether player is moving or attacking in a given frame
+    private void isMovingSetter() 
+    {
+        if(keyboard.isWASDPressed() || mouse.getMousePressed()) 
+            this.setIsMoving(true);
+        else 
+            this.setIsMoving(false);
+    }
 
     // Helper function: Determines player horizontal screen position
     private int playerScreenPositionX()
